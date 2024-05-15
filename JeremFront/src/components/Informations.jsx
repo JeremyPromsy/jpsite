3259baf1cef94bc284c21f1631167ff72d3d7c29
JeremyPromsy <jeremypromsy@gmail.com>
2024-05-15 18:25:08 +0200

    Avancement

--- JeremFront/src/components/Informations.jsx
+++ JeremFront/src/components/Informations.jsx
@@ -7,20 +7,27 @@ import imgJeremyPromsy from "../assets/Jérémy-Promsy.JPG";
 function Informations() {
     return (
       <div className="partInformations">
-        <div className="infoMoiPhoto">
-              <img 
-                className="imgJP" 
-                src={imgJeremyPromsy}
-                alt="Jérémy Promsy site web - developpeur front" />
-        </div>
+        <h2>Mieux me connaitre</h2>
         <div className="infoMoi">
-          <h2>A propos</h2>
-          <p> </p>
-        </div>
-        <div className="infoLiens">
-          <Link to="https://fr.linkedin.com/in/jeremypromsy" target="_blank" rel="noopener"><FontAwesomeIcon icon="fa-brands fa-linkedin" className="infoIcons infoIcons1"/></Link>
-          <Link to="https://twitter.com/JeremyPromsy" target="_blank" rel="noopener"><FontAwesomeIcon icon="fa-brands fa-twitter" className="infoIcons infoIcons2"/></Link>
-          <Link to="https://github.com/JeremyPromsy" target="_blank" rel="noopener"><FontAwesomeIcon icon="fa-brands fa-github" className="infoIcons infoIcons3" /></Link>
+          <div className="infoMoiPhoto">
+                <img 
+                  className="imgJP" 
+                  src={imgJeremyPromsy}
+                  alt="Jérémy Promsy site web - developpeur front" />
+          </div>
+          <div className="infoMoiTexte">
+            <p>Après un MBA en marketing digital, j'ai souhaité faire de mon appétence pour l'analyse, un nouveau défi. 
+              J'ai finalisé une formationde Data Analyst. 
+              Ayant eu une opportunité professionnelle de rejoindre une entreprise en tant que gestionnaire de projet digital.
+              Je l’ai saisi et eu la responsabilité de conduire le projet. 
+              J’ai en parallèle suivi et réussi une formation de développeur, venant ainsi compléter mon parcours. 
+              Ma mission s’étant terminée et riche de ces expériences, je suis aujourd'hui développeur front-end. </p>
+          </div>
+          <div className="infoLiens">
+            <Link to="https://fr.linkedin.com/in/jeremypromsy" target="_blank" rel="noopener"><FontAwesomeIcon icon="fa-brands fa-linkedin" className="infoIcons infoIcons1"/></Link>
+            <Link to="https://twitter.com/JeremyPromsy" target="_blank" rel="noopener"><FontAwesomeIcon icon="fa-brands fa-twitter" className="infoIcons infoIcons2"/></Link>
+            <Link to="https://github.com/JeremyPromsy" target="_blank" rel="noopener"><FontAwesomeIcon icon="fa-brands fa-github" className="infoIcons infoIcons3" /></Link>
+          </div>
         </div>
       </div>
     );
--- JeremFront/src/components/MeContacter.jsx
+++ JeremFront/src/components/MeContacter.jsx
@@ -5,8 +5,9 @@ import { NavLink } from "react-router-dom";
 function MeContacter() {
     return (
       <div className="partMeContacter">
+        <h2>Me contacter</h2>
         <div className="textMeContacter">
-          <p>Si vous voulez me contacter, partagez avec moi ou en savoir plus, <br></br>n'héhistez pas à me contacter</p>
+          <p>Si vous voulez me contacter, partagez avec moi ou en savoir plus, n'héhistez pas à me contacter</p>
         </div>
         <div className="boutonMeContacter">
           <NavLink to="/contact">Contact</NavLink>
